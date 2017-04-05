b6707bfc695becf48333e1fcd21c02443d2ff2b2
Kent Gauen <gauenk@purdue.edu>
2017-04-05 12:05:43 -0400

    mux added and initialized folders

---
+++ do_mapping.tcl
@@ -0,0 +1,25 @@
+# Step 1:  Read in the source file
+analyze -format sverilog -lib WORK { mux_4bits.sv}
+elaborate mux_4bits -lib WORK
+uniquify
+# Step 2: Set design constraints
+# Uncomment below to set timing, area, power, etc. constraints
+# set_max_delay <delay> -from "<input>" -to "<output>"
+# set_max_area <area>
+# set_max_total_power <power> mW
+ create_clock "clk" -name "clk" -period 5.0
+
+# Step 3: Compile the design
+compile -map_effort medium
+
+# Step 4: Output reports
+report_timing -path full -delay max -max_paths 1 -nworst 1 > reports/mux_4bits.rep
+report_area >> reports/mux_4bits.rep
+report_power -hier >> reports/mux_4bits.rep
+
+# Step 5: Output final VHDL and Verilog files
+write_file -format verilog -hierarchy -output "mapped/mux_4bits.v"
+echo "\nScript Done\n"
+echo "\nChecking Design\n"
+check_design
+quit
---
+++ source/enc_byte_sub.sv~
@@ -0,0 +1,17 @@
+module byte_sub
+  (
+   input [127:0] lu_table,
+   input [7:0] data_i,
+   output [7:0]	 data_o
+   );
+
+   always_comb begin : MULTIPLEXER
+      case(data_i)
+	
+
+      endcase
+     end
+
+   
+     
+endmodule
