181c89f424e8b63ad88294ffeefb6abee6e1169a
Deeptanshu Malik <DeeptanshuM@users.noreply.github.com>
2017-04-23 13:17:26 -0400

    starting top level tb

---
+++ source/tb_top_level.sv
@@ -0,0 +1,20 @@
+// $Id: $
+// File name:   tb_top_level.sv
+// Created:     4/23/2017
+// Author:      Deeptanshu Malik
+// Lab Section: 337-03
+// Version:     1.0  Initial Design Entry
+// Description: Top level test bench.
+
+`timescale 1ns / 10ps
+module tb_top_level();
+
+//gameplan is to follow flowchart for top level design
+
+/*
+1. Send data from master
+
+2. Demonstrate that ahb is functioning - DrJ said so
+
+3. Encrypt/Decrypt data from fifo - multiple examples of this
+*/
